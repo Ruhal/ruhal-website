f9202b04f9fe74857e5aafbe00ceed6ee30560a6
ruhal <ruhalmiah@gmail.com>
2025-03-16 13:07:06 +0000

    fix 16

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
-import remarkUnwrapImages from 'remark-unwrap-images';
-import rehypeExternalLinks from 'rehype-external-links';
-import expressiveCode from 'astro-expressive-code';
-import { expressiveCodeOptions } from './src/site.config';
-import icon from 'astro-icon';
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://www.ruhal.co.uk',
-    integrations: [expressiveCode(expressiveCodeOptions), tailwind({
-        applyBaseStyles: false
-    }), sitemap(), mdx(), icon()],
-    markdown: {
-        remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-        rehypePlugins: [[rehypeExternalLinks, {
-            target: '_blank',
-            rel: ['nofollow, noopener, noreferrer']
-        }]],
-        remarkRehype: {
-            footnoteLabelProperties: {
-                className: ['']
-            }
-        }
-    },
-    prefetch: true,
-    output: "server",
-    adapter: cloudflare()
+    site: 'https://www.ruhal.co.uk',
 });
